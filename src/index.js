d37d2e3be58b3db0fb87f10cfbfb46552a602971
Elyrene <3222956277@qq.com>
2026-05-03 14:16:39 +0800

    🐳 chore: 修复旧版构建流程报错问题，改用新版

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
-render(<App />, document.getElementById('app'));
+//获取 DOM 容器
+const container = document.getElementById('app');
+
+//创建根
+const root = createRoot(container);
+
+//渲染应用
+root.render(<App />);
 
 if (module.hot) {
   module.hot.accept(function() {
